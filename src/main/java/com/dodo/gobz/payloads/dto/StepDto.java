9acdf6c9c6b7e42d68a8703ddd17b38cc8882c0d
Donovan Persent <donovan.persent@noconsulting.fr>
2021-05-10 18:20:08 +0200

    :sparkles: Add completion

--- src/main/java/com/dodo/gobz/payloads/dto/StepDto.java
+++ src/main/java/com/dodo/gobz/payloads/dto/StepDto.java
@@ -2,12 +2,10 @@ package com.dodo.gobz.payloads.dto;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class StepDto {
     private Long id;
     private String name;
     private String description;
-    private List<TaskDto> tasks;
+    private float completion;
 }
