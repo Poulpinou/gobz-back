334b5d95b8aa90534ef602c759e1cb1aac83b0eb
Donovan Persent <donovan.persent@noconsulting.fr>
2021-05-19 12:11:35 +0200

    📦️ Dockerize

--- src/test/java/com/dodo/gobz/GobzBackApplicationTests.java
+++ src/test/java/com/dodo/gobz/GobzBackApplicationTests.java
@@ -3,7 +3,7 @@ package com.dodo.gobz;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GobzBackApplicationTests {
 
 	@Test
